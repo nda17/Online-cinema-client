c19e9a6c4a3e86e251f2341a691ce3249437481d
nfdmitry <khv1702@gmail.com>
2024-04-03 20:00:04 +0300

    Editing field headers in table columns of the Admin panel

--- src/components/ui/admin-table/AdminTable/AdminTableHeader.tsx
+++ src/components/ui/admin-table/AdminTable/AdminTableHeader.tsx
@@ -10,7 +10,7 @@ const AdminTableHeader: FC<{ headerItems: string[] }> = ({
 			{headerItems.map((value) => (
 				<div key={value}>{value}</div>
 			))}
-			<div>Actions</div>
+			<div>Actions:</div>
 		</div>
 	)
 }
