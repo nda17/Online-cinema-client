4957468fd597d9d0d24edbba4fd14fd23fe895e4
nfdmitry <khv1702@gmail.com>
2024-04-03 19:41:23 +0300

    Added Movies  section in the Admin Panel

---
+++ src/components/screens/admin/movies/useMovies.ts
@@ -0,0 +1,70 @@
+import { ADMIN_URL } from '@/configs/url.config'
+import { useDebounce } from '@/hooks/useDebounce'
+import { MovieService } from '@/services/movie/movie.service'
+import { ITableItem } from '@/ui/admin-table/AdminTable/admin-table.interface'
+import { toastrError } from '@/utils/api/toastr-error-redux'
+import { getGenresList } from '@/utils/movie/getGenresList'
+import { ChangeEvent, MouseEvent, useMemo, useState } from 'react'
+import { useMutation, useQuery } from 'react-query'
+import { toastr } from 'react-redux-toastr'
+
+export const useMovies = () => {
+	const [searchTerm, setSearchTerm] = useState('')
+	const debouncedSearch = useDebounce(searchTerm, 500)
+
+	const queryData = useQuery(
+		['movie list', debouncedSearch],
+		() => MovieService.getMoviesList(debouncedSearch),
+		{
+			select: ({ data }) =>
+				data.map(
+					(movie): ITableItem => ({
+						_id: movie._id,
+						editUrl: ADMIN_URL.rootUrl(`movie/edit/${movie._id}`),
+						items: [
+							movie.title,
+							getGenresList(movie.genres),
+							String(movie.rating)
+						]
+					})
+				),
+
+			onError: (error) => {
+				toastrError(error, 'Movie list')
+			}
+		}
+	)
+
+	const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+		setSearchTerm(e.target.value)
+	}
+
+	const handleClear = (e: MouseEvent<HTMLSpanElement>) => {
+		setSearchTerm('')
+	}
+
+	const { mutateAsync: deleteAsync } = useMutation(
+		'delete movie',
+		(movieId: string) => MovieService.deleteMovie(movieId),
+		{
+			onError(error) {
+				toastrError(error, 'Delete movie')
+			},
+			onSuccess() {
+				toastr.success('Delete movie', 'delete was successful')
+				queryData.refetch()
+			}
+		}
+	)
+
+	return useMemo(
+		() => ({
+			handleSearch,
+			handleClear,
+			...queryData,
+			searchTerm,
+			deleteAsync
+		}),
+		[queryData, searchTerm, deleteAsync]
+	)
+}
