f66e9fe80224ebf055ee9fcd59d55e5e635b4770
nfdmitry <khv1702@gmail.com>
2024-04-11 03:11:07 +0300

    Added hook click outside window

---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,22 @@
+import { useEffect } from 'react'
+
+const useClickOutside = (
+	ref: React.MutableRefObject<HTMLElement | null>,
+	callback: () => void
+) => {
+	const handleClick = (event: MouseEvent) => {
+		if (ref.current && !ref.current.contains(event.target as Node)) {
+			callback()
+		}
+	}
+
+	useEffect(() => {
+		document.addEventListener('mousedown', handleClick)
+
+		return () => {
+			document.removeEventListener('mousedown', handleClick)
+		}
+	}, [])
+}
+
+export { useClickOutside }
