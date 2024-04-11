012808da613cd9e828b71b611fd3e5184e42b3c5
nfdmitry <khv1702@gmail.com>
2024-04-12 00:03:18 +0300

    Adding a reducers for extra menu and hamburger

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,8 +1,12 @@
 import { reducer as toastrReducer } from 'react-redux-toastr'
+import { reducer as extraMenuReducer } from './extra-menu/extra-menu.slice'
+import { reducer as hamburgerReducer } from './hamburger/hamburger.slice'
 import { reducer as popupReducer } from './popup/popup.slice'
 import { reducer as userReducer } from './user/user.slice'
 
 export const reducers = {
+	hamburger: hamburgerReducer,
+	extraMenu: extraMenuReducer,
 	user: userReducer,
 	popup: popupReducer,
 	toastr: toastrReducer
