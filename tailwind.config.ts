ed25c7a43bdec1d89f71645aab23fd5f1005caf4
nfdmitry <khv1702@gmail.com>
2024-04-18 01:05:39 +0300

    Adding responsiveness to layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -63,6 +63,7 @@ module.exports = {
 			},
 
 			boxShadow: {
+				black: '0px 0px 8px 1px rgba(0, 0, 0, 0.39)',
 				xs: '0 0 0 1px rgba(0, 0, 0, 0.05)',
 				sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
 				md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
