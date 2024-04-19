e24c73faf249472e323ad2e0316b5478de6f4b0c
nfdmitry <khv1702@gmail.com>
2024-04-19 14:30:48 +0300

    Fix bugs

--- src/app/admin/genre/edit/[id]/page.tsx
+++ src/app/admin/genre/edit/[id]/page.tsx
@@ -1,8 +1,8 @@
 import GenreEdit from '@/screens/admin/genre/GenreEdit'
-import { IGenreId } from '@/screens/admin/genre/genre-edit.interface'
+import { IParamsUrl } from '@/shared/types/params-url.types'
 import { NextPage } from 'next'
 
-const GenreEditPage: NextPage<IGenreId> = ({ params }) => {
+const GenreEditPage: NextPage<IParamsUrl> = ({ params }) => {
 	return <GenreEdit params={params} />
 }
 
