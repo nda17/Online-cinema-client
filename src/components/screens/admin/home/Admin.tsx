89ab8dbfc9f3c9fb2ae771bd3f64780b2c9d2773
nfdmitry <khv1702@gmail.com>
2024-04-22 14:06:25 +0300

    Updated some styles, added a bounding container for the admin panel

--- src/components/screens/admin/home/Admin.tsx
+++ src/components/screens/admin/home/Admin.tsx
@@ -2,12 +2,14 @@ import AdminNavigation from '@/ui/admin-navigation/AdminNavigation'
 import Heading from '@/ui/heading/Heading'
 import { FC } from 'react'
 import Statistics from './Statistics/Statistics'
+import adminWrapper from '@/components/shared/admin/adminWrapper.module.scss'
+
 
 const Admin: FC = () => {
 	return (
 		<>
 			<AdminNavigation />
-			<div className={'wrapper'}>
+			<div className={adminWrapper.wrapper}>
 				<Heading title="Some statistics" />
 				<Statistics />
 			</div>
