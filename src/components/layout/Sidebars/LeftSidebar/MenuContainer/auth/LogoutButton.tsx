8834ed39989630d7e6b9de030d018bb313fb5c89
nfdmitry <khv1702@gmail.com>
2024-04-22 15:08:35 +0300

    Added redirect to login page when logging out of profile

--- src/components/layout/Sidebars/LeftSidebar/MenuContainer/auth/LogoutButton.tsx
+++ src/components/layout/Sidebars/LeftSidebar/MenuContainer/auth/LogoutButton.tsx
@@ -1,14 +1,17 @@
 import { useActions } from '@/hooks/useActions'
 import MaterialIcon from '@/ui/icons/MaterialIcon'
+import { useRouter } from 'next/navigation'
 import { FC, MouseEvent } from 'react'
 import styles from '../Menu.module.scss'
 
 const LogoutButton: FC = () => {
 	const { logout } = useActions()
+	const { push } = useRouter()
 
 	const logoutHandler = (e: MouseEvent<HTMLAnchorElement>) => {
 		e.preventDefault()
 		logout()
+		push('/auth')
 	}
 
 	return (
