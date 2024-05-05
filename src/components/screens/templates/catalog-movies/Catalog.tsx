2e8be229066f898584c78a4cc5aa99bde1474d6e
nfdmitry <khv1702@gmail.com>
2024-05-05 17:52:35 +0300

    Added pagination of displayed elements from the Catalog

--- src/components/screens/templates/catalog-movies/Catalog.tsx
+++ src/components/screens/templates/catalog-movies/Catalog.tsx
@@ -1,12 +1,44 @@
+'use client'
 import { PUBLIC_URL } from '@/configs/url.config'
 import Description from '@/ui/description/Description'
 import GalleryItem from '@/ui/gallery/GalleryItem'
 import Heading from '@/ui/heading/Heading'
-import { FC } from 'react'
+import Pagination from '@/ui/pagination/Pagination'
+import { FC, useState } from 'react'
 import styles from './Catalog.module.scss'
 import { ICatalog } from './catalog.interface'
 
 const Catalog: FC<ICatalog> = ({ title, description, movies }) => {
+	const [currentPage, setCurrentPage] = useState(1) //Текущая страница
+	const itemQuantity = 8 //Количество элементов в одной группе
+
+	const lastCardIndex = currentPage * itemQuantity //Индекс последнего элемента
+	const firstCardIndex = lastCardIndex - itemQuantity //Индекс первого элемента
+	const activePage = movies.slice(firstCardIndex, lastCardIndex)
+	const listPage = [] //Количество отображаемых групп с элементами
+	for (let i = 1; i <= Math.ceil(movies.length / itemQuantity); i++) {
+		listPage.push(i)
+	}
+
+	const prevPage = () => {
+		if (currentPage !== 1) {
+			setCurrentPage((prev) => prev - 1)
+		} else {
+			return
+		}
+	}
+
+	const changeActivePage = (activePage: number) =>
+		setCurrentPage(activePage)
+
+	const nextPage = () => {
+		if (currentPage !== Math.ceil(movies.length / itemQuantity)) {
+			setCurrentPage((prev) => prev + 1)
+		} else {
+			return
+		}
+	}
+
 	return (
 		<>
 			<Heading title={title} />
@@ -14,7 +46,7 @@ const Catalog: FC<ICatalog> = ({ title, description, movies }) => {
 				<Description text={description} className={styles.description} />
 			)}
 			<div className={styles.movies}>
-				{movies.map((movie) => (
+				{activePage.map((movie) => (
 					<GalleryItem
 						key={movie._id}
 						variant="horizontal"
@@ -29,6 +61,15 @@ const Catalog: FC<ICatalog> = ({ title, description, movies }) => {
 					/>
 				))}
 			</div>
+			{movies.length > itemQuantity && (
+				<Pagination
+					listPage={listPage}
+					currentPage={currentPage}
+					prevPage={prevPage}
+					nextPage={nextPage}
+					changeActivePage={changeActivePage}
+				/>
+			)}
 		</>
 	)
 }
