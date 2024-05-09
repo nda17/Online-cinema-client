1e66471ab15281537d7ea38a054cf631cfa31ba9
nfdmitry <khv1702@gmail.com>
2024-05-10 01:33:31 +0300

    Added user requests to update data in a personal profile

--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -1,9 +1,23 @@
 import { PUBLIC_PATH } from '@/configs/api.config'
 import { IUserEditInput } from '@/screens/admin/user/user-edit.interface'
+import { IProfileInput } from '@/screens/profile/profile.interface'
 import { IUser } from '@/shared/types/user.types'
 import axiosInterceptorsRequest from 'api/interceptors'
 
 export const UserService = {
+	async getProfile() {
+		return axiosInterceptorsRequest.get<IUser>(
+			PUBLIC_PATH.usersUrl('/profile')
+		)
+	},
+
+	async updateProfile(data: IProfileInput) {
+		return axiosInterceptorsRequest.put<string>(
+			PUBLIC_PATH.usersUrl('/profile'),
+			data
+		)
+	},
+
 	async getUsersList(searchTerm?: string) {
 		return axiosInterceptorsRequest.get<IUser[]>(
 			PUBLIC_PATH.usersUrl(``),
