2f3cb5db19e5f1af4e397502a7509b456e075b60
nfdmitry <khv1702@gmail.com>
2024-05-13 22:05:42 +0300

    Added a custom video player to the movie page

--- src/components/screens/single-movie/SingleMovie.tsx
+++ src/components/screens/single-movie/SingleMovie.tsx
@@ -3,9 +3,17 @@ import styles from '@/components/shared/contentWrapper.module.scss'
 import Banner from '@/ui/banner/Banner'
 import Gallery from '@/ui/gallery/Gallery'
 import SubHeading from '@/ui/subheading/SubHeading'
+import dynamic from 'next/dynamic'
 import { FC } from 'react'
 import Content from './Content/Content'
 import { IMoviePage } from './single-movie.interface'
+
+const DynamicPlayer = dynamic(
+	() => import('@/ui/video-player/VideoPlayer'),
+	{
+		ssr: false
+	}
+)
 
 const SingleMovie: FC<IMoviePage> = ({ movie, similarMovies }) => {
 	return (
@@ -14,6 +22,8 @@ const SingleMovie: FC<IMoviePage> = ({ movie, similarMovies }) => {
 				imagePath={movie.bigPoster}
 				Detail={() => <Content movie={movie} />}
 			/>
+
+			<DynamicPlayer videoSource={movie.videoUrl} slug={movie.slug} />
 
 			<>
 				<SubHeading title="Similar movies" />
