e9536643fb826ac0ac827b40abba1e4a8b92bbbd
nfdmitry <khv1702@gmail.com>
2024-05-17 00:11:58 +0300

    Added checking the conditions for watching a movie (free/subscription), displaying a placeholder if there is no subscription

--- src/components/ui/video-player/VideoPlayer.tsx
+++ src/components/ui/video-player/VideoPlayer.tsx
@@ -1,99 +1,116 @@
 import { useAuth } from '@/hooks/useAuth'
+import { UserService } from '@/services/user/user.service'
 import classNames from 'classnames'
 import { FC } from 'react'
+import { useQuery } from 'react-query'
 import MaterialIcon from '../icons/MaterialIcon'
-import AuthPlaceholder from './AuthPlaceholder/AuthPlaceholder'
 import ProgressBar from './ProgressBar/ProgressBar'
 import styles from './VideoPlayer.module.scss'
+import AuthPlaceholder from './placeholder-screens/AuthPlaceholder/AuthPlaceholder'
+import SubscriptionPlaceholder from './placeholder-screens/SubscriptionPlaceholder/SubscriptionPlaceholder'
 import { useVideo } from './useVideo'
 import { IVideoPlayer } from './video.types'
 
-const VideoPlayer: FC<IVideoPlayer> = ({ videoSource, slug }) => {
+const VideoPlayer: FC<IVideoPlayer> = ({ videoSource, license, slug }) => {
 	const { actions, videoRef, video } = useVideo()
 	const { user } = useAuth()
+
+	const { data } = useQuery(
+		'get subscription user profile',
+		() => UserService.getProfile(),
+		{
+			select: (data) => data.data.isSubscription
+		}
+	)
 
 	return (
 		<div
 			className={classNames(styles.player, {
-				'h-96': !user
+				'h-96': !user || (!data && license === 'subscription')
 			})}
 		>
 			{user ? (
-				<>
-					<video
-						ref={videoRef}
-						className={styles.video}
-						src={`${videoSource}#t=4`}
-						preload="metadata"
-					/>
+				!data && license === 'subscription' ? (
+					<SubscriptionPlaceholder slug={slug} />
+				) : (
+					<>
+						<video
+							ref={videoRef}
+							className={styles.video}
+							src={`${videoSource}#t=4`}
+							preload="metadata"
+						/>
 
-					{video.currentTime === video.videoTime && (
-						<button
-							onClick={actions.replay}
-							className={styles.buttonReplay}
-						>
-							<MaterialIcon name="MdRestartAlt" />
-						</button>
-					)}
-
-					<ProgressBar
-						min={0.4}
-						max={video.videoTime - 1.19425}
-						value={video.currentTime}
-						onChange={(e: any) =>
-							actions.changeTime(Number(e.target.value))
-						}
-					/>
-
-					<div className={styles.controls}>
-						<div>
-							<button onClick={actions.fastRevert}>
-								<MaterialIcon name="MdHistory" />
-							</button>
-
+						{video.currentTime === video.videoTime && (
 							<button
-								onClick={actions.toggleVideo}
-								className={styles.playButton}
+								onClick={actions.replay}
+								className={styles.buttonReplay}
 							>
-								<MaterialIcon
-									name={video.isPlaying ? 'MdPause' : 'MdPlayArrow'}
-								/>
+								<MaterialIcon name="MdRestartAlt" />
 							</button>
+						)}
 
-							<button onClick={actions.fastForward}>
-								<MaterialIcon name="MdUpdate" />
-							</button>
+						<ProgressBar
+							min={0.4}
+							max={video.videoTime - 1.19425}
+							value={video.currentTime}
+							onChange={(e: any) =>
+								actions.changeTime(Number(e.target.value))
+							}
+						/>
 
-							<button onClick={actions.mute}>
-								<MaterialIcon
-									name={
-										videoRef.current?.muted ? 'MdVolumeOff' : 'MdVolumeUp'
-									}
-								/>
-							</button>
+						<div className={styles.controls}>
+							<div>
+								<button onClick={actions.fastRevert}>
+									<MaterialIcon name="MdHistory" />
+								</button>
 
-							<div className={styles.timeControls}>
-								<p className={styles.controlsTime}>
-									{Math.floor(video.currentTime / 60) +
-										':' +
-										('0' + Math.floor(video.currentTime % 60)).slice(-2)}
-								</p>
-								<p> / </p>
-								<p className={styles.controlsTime}>
-									{Math.floor(video.videoTime / 60) +
-										':' +
-										('0' + Math.floor(video.videoTime % 60)).slice(-2)}
-								</p>
+								<button
+									onClick={actions.toggleVideo}
+									className={styles.playButton}
+								>
+									<MaterialIcon
+										name={video.isPlaying ? 'MdPause' : 'MdPlayArrow'}
+									/>
+								</button>
+
+								<button onClick={actions.fastForward}>
+									<MaterialIcon name="MdUpdate" />
+								</button>
+
+								<button onClick={actions.mute}>
+									<MaterialIcon
+										name={
+											videoRef.current?.muted
+												? 'MdVolumeOff'
+												: 'MdVolumeUp'
+										}
+									/>
+								</button>
+
+								<div className={styles.timeControls}>
+									<p className={styles.controlsTime}>
+										{Math.floor(video.currentTime / 60) +
+											':' +
+											('0' + Math.floor(video.currentTime % 60)).slice(-2)}
+									</p>
+									<p> / </p>
+									<p className={styles.controlsTime}>
+										{Math.floor(video.videoTime / 60) +
+											':' +
+											('0' + Math.floor(video.videoTime % 60)).slice(-2)}
+									</p>
+								</div>
+							</div>
+
+							<div>
+								<button onClick={actions.fullScreen}>
+									<MaterialIcon name="MdFullscreen" />
+								</button>
 							</div>
 						</div>
-
-						<div>
-							<button onClick={actions.fullScreen}>
-								<MaterialIcon name="MdFullscreen" />
-							</button>
-						</div>
-					</div>
-				</>
+					</>
+				)
 			) : (
 				<AuthPlaceholder slug={slug} />
 			)}
