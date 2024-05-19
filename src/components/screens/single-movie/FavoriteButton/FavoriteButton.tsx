cc078c62e40d0197805b4dbcf2f74b1000e5f4b2
nfdmitry <khv1702@gmail.com>
2024-05-19 23:03:34 +0300

    Implemented a button component for adding/removing from the list of favorite films

---
+++ src/components/screens/single-movie/FavoriteButton/FavoriteButton.tsx
@@ -0,0 +1,49 @@
+'use client'
+import { UserService } from '@/services/user/user.service'
+import { toastrError } from '@/utils/api/toastr-error-redux'
+import classNames from 'classnames'
+import { FC, useEffect, useState } from 'react'
+import { useMutation } from 'react-query'
+import { useFavorites } from '../../favorites/useFavorites'
+import styles from './FavoriteButton.module.scss'
+import HeartImage from './heart-animation.png'
+
+const FavoriteButton: FC<{ movieId: string }> = ({ movieId }) => {
+	const [isSmashed, setIsSmashed] = useState(false)
+
+	const { movies, refetch } = useFavorites()
+
+	useEffect(() => {
+		if (movies) {
+			const isHasMovie = movies.some((movie) => movie._id === movieId)
+			if (isSmashed !== isHasMovie) setIsSmashed(isHasMovie)
+		}
+	}, [movies, isSmashed, movieId])
+
+	const { mutateAsync } = useMutation(
+		'update favorites movies',
+		() => UserService.toggleFavorite(movieId),
+		{
+			onSuccess() {
+				setIsSmashed(!isSmashed)
+				refetch()
+			},
+
+			onError(error) {
+				toastrError(error, 'Update favorite list')
+			}
+		}
+	)
+
+	return (
+		<button
+			onClick={() => mutateAsync()}
+			className={classNames(styles.button, {
+				[styles.animate]: isSmashed,
+			})}
+			style={{ backgroundImage: `url(${HeartImage.src})` }}
+		/>
+	)
+}
+
+export default FavoriteButton
