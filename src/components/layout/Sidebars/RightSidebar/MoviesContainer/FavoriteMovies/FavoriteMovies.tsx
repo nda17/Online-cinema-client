e7d30e3b6ed4656fa72e903106638a8db59405d2
nfdmitry <khv1702@gmail.com>
2024-05-20 15:37:12 +0300

    Added the FavoriteBanner.tsx component to display an information banner depending on the authorization condition/availability of products in favorites.

--- src/components/layout/Sidebars/RightSidebar/MoviesContainer/FavoriteMovies/FavoriteMovies.tsx
+++ src/components/layout/Sidebars/RightSidebar/MoviesContainer/FavoriteMovies/FavoriteMovies.tsx
@@ -4,19 +4,20 @@ import { useFavorites } from '@/screens/favorites/useFavorites'
 import SkeletonLoader from '@/ui/skeleton-loader/SkeletonLoader'
 import { FC } from 'react'
 import MovieList from '../MovieList'
-import NotAuthFavorites from './NotAuthFavorites'
+import FavoriteBanner from './FavoriteBanner/FavoriteBanner'
 
 const FavoriteMovies: FC = () => {
 	const { isLoading, movies } = useFavorites()
 	const { user } = useAuth()
-	
+	const isAuth = user ? true : false
+
 	return isLoading ? (
 		<div className="mt-11">
 			<SkeletonLoader count={3} className="h-28 mb-4" />
 		</div>
 	) : (
-		!isLoading && !user ? (
-			<NotAuthFavorites />
+		!isLoading && !isAuth || !isLoading && !movies?.length ? (
+			<FavoriteBanner isAuth={isAuth} />
 		) : (<MovieList
 				link='/favorites'
 				movies={movies?.slice(0, 3) || []}
