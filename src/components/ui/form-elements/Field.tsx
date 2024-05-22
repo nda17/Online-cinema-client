1fa920167f2c38389744f43d6bb68d071ba9294e
nfdmitry <khv1702@gmail.com>
2024-05-22 23:16:10 +0300

    Fix bugs

--- src/components/ui/form-elements/Field.tsx
+++ src/components/ui/form-elements/Field.tsx
@@ -2,6 +2,7 @@ import { forwardRef } from 'react'
 import { IField } from './form.interface'
 import styles from './form.module.scss'
 
+/* eslint-disable react/display-name */
 const Field = forwardRef<HTMLInputElement, IField>(
 	({ placeholder, error, type = 'text', style, ...rest }, ref) => {
 		return (
