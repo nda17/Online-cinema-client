741b383984ee5f193dfb8672fcbbc4c805a4878d
nfdmitry <khv1702@gmail.com>
2024-05-24 21:25:48 +0300

    Changing the logic of behavior with a transition to the page of selected films in the absence of authorization. Instead of a page, a 404 page is displayed

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,13 +1,19 @@
-import Favorites from '@/screens/favorites/Favorites'
 import { Metadata, NextPage } from 'next'
+import dynamic from 'next/dynamic'
+
+const DynamicFavorites = dynamic(
+	() => import('@/screens/favorites/Favorites'),
+	{
+		ssr: false
+	}
+)
 
 export const metadata: Metadata = {
 	title: 'Favorites | Online-Cinema'
 }
 
 const FavoritePage: NextPage = () => {
-	return <Favorites />
+	return <DynamicFavorites />
 }
 
 export default FavoritePage
-
--- src/components/screens/favorites/Favorites.tsx
+++ src/components/screens/favorites/Favorites.tsx
@@ -1,5 +1,7 @@
 'use client'
+import Error404 from '@/app/not-found'
 import stylesWrapper from '@/components/shared/contentWrapper.module.scss'
+import { useAuth } from '@/hooks/useAuth'
 import Description from '@/ui/description/Description'
 import Heading from '@/ui/heading/Heading'
 import SkeletonLoader from '@/ui/skeleton-loader/SkeletonLoader'
@@ -9,34 +11,41 @@ import styles from './Favorites.module.scss'
 import { useFavorites } from './useFavorites'
 
 const Favorites: FC = () => {
+	const { user } = useAuth()
 	const { movies, isLoading } = useFavorites()
 
 	const title = 'Favorites'
 	const description =
 		"Featured films page. No need to search for your favorite movie, you can immediately start watching it anytime, anywhere'"
-	const text =
-		'Featured films page. No need to search for your favorite movie, you can immediately start watching it anytime, anywhere'
+
 	return (
 		<div className={stylesWrapper.contentWrapper}>
-			{isLoading ? (
-				<>
-					<Heading title={'Favorites'} />
-					<Description text={text} className={styles.description} />
-					<SkeletonLoader
-						count={9}
-						className={styles.skeletonLoader}
-						containerClassName={styles.containerLoader}
-					/>
-				</>
+			{user ? (
+				isLoading ? (
+					<>
+						<Heading title={'Favorites'} />
+						<Description
+							text={description}
+							className={styles.description}
+						/>
+						<SkeletonLoader
+							count={9}
+							className={styles.skeletonLoader}
+							containerClassName={styles.containerLoader}
+						/>
+					</>
+				) : (
+					<>
+						<CatalogMovies
+							movies={movies || []}
+							title={title}
+							description={description}
+							favorite={true}
+						/>
+					</>
+				)
 			) : (
-				<>
-					<CatalogMovies
-						movies={movies || []}
-						title={title}
-						description={description}
-						favorite={true}
-					/>
-				</>
+				<Error404 />
 			)}
 		</div>
 	)
