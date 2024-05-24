28d10e67a3222e3f459054ada7aaaf822be9f337
nfdmitry <khv1702@gmail.com>
2024-05-24 17:55:14 +0300

    Fixed a bug with executing a favorites request when there is no authorization

--- src/components/screens/favorites/useFavorites.ts
+++ src/components/screens/favorites/useFavorites.ts
@@ -1,13 +1,17 @@
+import { useAuth } from '@/hooks/useAuth'
 import { UserService } from '@/services/user/user.service'
 import { useQuery } from 'react-query'
 
 export const useFavorites = () => {
+	const { user } = useAuth()
+
 	const {
 		isLoading,
 		data: movies,
-		refetch,
+		refetch
 	} = useQuery('Favorite movies', () => UserService.getFavorites(), {
-		select: ({ data }) => data
+		select: ({ data }) => data,
+		enabled: !!user
 	})
 
 	return { isLoading, movies, refetch }
