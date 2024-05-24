a219396519996d8dd6375c9a4d7d7e67b9ee4fcc
nfdmitry <khv1702@gmail.com>
2024-05-25 00:28:29 +0300

    Added a section with a catalog of genres

---
+++ src/app/genre-catalog/page.tsx
@@ -0,0 +1,44 @@
+import Collection from '@/screens/collection/Collection'
+import { GenreService } from '@/services/genre/genre.service'
+import { IGenresCollection } from '@/shared/types/collection.types'
+import { errorCatch } from 'api/api.helpers'
+import { Metadata } from 'next'
+import Error404 from '../not-found'
+
+export const metadata: Metadata = {
+	title: 'Genre catalog | Online-Cinema'
+}
+
+const GenreCatalogPage = async () => {
+	const data = await staticContent()
+
+	const collection = data?.collection
+
+	return collection ? (
+		<Collection collection={collection || []} />
+	) : (
+		<Error404 />
+	)
+}
+
+//Genre catalog fetch
+export const staticContent = async () => {
+	try {
+		const response = await GenreService.getCollections()
+
+		const collection: IGenresCollection[] = response.data.map((item) => ({
+			_id: item._id,
+			image: item.image,
+			title: item.title,
+			slug: item.slug
+		}))
+
+		return {
+			collection
+		}
+	} catch (error) {
+		console.log(errorCatch(error))
+	}
+}
+
+export default GenreCatalogPage
