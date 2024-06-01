69c7997fd09b3430d2205d26e7419f5e3165d54f
nfdmitry <khv1702@gmail.com>
2024-06-02 02:58:35 +0300

    Hydration bug fixed

--- src/components/layout/Mobile/MobileNavbar/Navigation/Navigation.tsx
+++ src/components/layout/Mobile/MobileNavbar/Navigation/Navigation.tsx
@@ -1,5 +1,3 @@
-'use client'
-import { useAuth } from '@/hooks/useAuth'
 import FavoritesMenu from '@/ui/favorites-menu/FavoritesMenu'
 import Hamburger from '@/ui/hamburger/Hamburger'
 import { FC } from 'react'
@@ -8,8 +6,6 @@ import styles from './Navigation.module.scss'
 import SearchContainer from './SearchContainer/SearchContainer'
 
 const Navigation: FC = () => {
-	const { user } = useAuth()
-
 	return (
 		<header className={styles.navigation}>
 			<Hamburger />
@@ -17,7 +13,7 @@ const Navigation: FC = () => {
 				<Logo />
 				<SearchContainer />
 			</div>
-			{user && <FavoritesMenu />}
+			<FavoritesMenu />
 		</header>
 	)
 }
