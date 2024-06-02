f1d79af0b094a299b839c46d2ad37805989052a2
nfdmitry <khv1702@gmail.com>
2024-06-03 01:54:00 +0300

    Removed unnecessary rendering of the "Description" component, transferred the "device" entity to the "CatalogMovies" component

--- src/components/screens/favorites/Favorites.tsx
+++ src/components/screens/favorites/Favorites.tsx
@@ -2,46 +2,39 @@
 import FavoriteBanner from '@/components/layout/Sidebars/RightSidebar/MoviesContainer/FavoriteMovies/FavoriteBanner/FavoriteBanner'
 import stylesWrapper from '@/components/shared/contentWrapper.module.scss'
 import { useAuth } from '@/hooks/useAuth'
-import Description from '@/ui/description/Description'
 import Heading from '@/ui/heading/Heading'
 import SkeletonLoader from '@/ui/skeleton-loader/SkeletonLoader'
 import { FC } from 'react'
 import CatalogMovies from '../templates/catalog-movies/CatalogMovies'
 import styles from './Favorites.module.scss'
+import { IFavorites } from './favorites.interface'
 import { useFavorites } from './useFavorites'
 
-const Favorites: FC = () => {
+const Favorites: FC<IFavorites> = ({ device }) => {
 	const { user } = useAuth()
 	const { movies, isLoading } = useFavorites()
-
 	const title = 'Favorites'
-	const description =
-		"Featured films page. No need to search for your favorite movie, you can immediately start watching it anytime, anywhere'"
-
 
 	return (
 		<div className={stylesWrapper.contentWrapper}>
-			<Heading title={'Favorites'} />
+			<Heading title={title} />
 			{user ? (
 				isLoading ? (
 					<>
-						<Description
-							text={description}
-							className={styles.description}
-						/>
 						<SkeletonLoader
 							count={9}
 							className={styles.skeletonLoader}
 							containerClassName={styles.containerLoader}
 						/>
 					</>
-				) : (
-					movies?.length ? <CatalogMovies
+				) : movies?.length ? (
+					<CatalogMovies
 						movies={movies || []}
-						title={''}
-						description={''}
 						favorite={true}
-					/> : <FavoriteBanner isAuth={true} />
+						device={device}
+					/>
+				) : (
+					<FavoriteBanner isAuth={true} />
 				)
 			) : (
 				<FavoriteBanner isAuth={false} />
