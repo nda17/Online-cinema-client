fb977093717af260c9a5255be17d41c9e00fe4d3
nfdmitry <khv1702@gmail.com>
2024-06-02 21:57:26 +0300

    Updated server paths

--- next.config.js
+++ next.config.js
@@ -3,15 +3,18 @@
 const nextConfig = {
 	reactStrictMode: false,
 	poweredByHeader: false,
+	env: {
+		APP_URL: process.env.REACT_APP_URL
+	},
 	async rewrites() {
 		return [
 			{
 				source: '/api/:path*',
-				destination: `http://localhost:4200/api/:path*`
+				destination: `http://92.118.113.223:4200/api/:path*`
 			},
 			{
 				source: '/uploads/:path*',
-				destination: `http://localhost:4200/uploads/:path*`
+				destination: `http://92.118.113.223:4200/uploads/:path*`
 			}
 		]
 	}
