975c1fc030d78260886734b1d27461aa7586c4a3
nfdmitry <khv1702@gmail.com>
2024-06-04 23:52:36 +0300

    Added rating typing

--- src/components/ui/slider/slider.interface.ts
+++ src/components/ui/slider/slider.interface.ts
@@ -1,7 +1,7 @@
 import { IMovie } from '@/shared/types/movie.types'
 
 export interface ISlide
-	extends Pick<IMovie, '_id' | 'title' | 'bigPoster'> {
+	extends Pick<IMovie, '_id' | 'title' | 'bigPoster' | 'rating'> {
 	subTitle: string
 	url: string
 }
