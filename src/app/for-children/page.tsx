f45adfa3f92942b4e12df450ed3d22d48456e5c4
nfdmitry <khv1702@gmail.com>
2024-06-04 22:47:17 +0300

    Added a section "For children"

---
+++ src/app/for-children/page.tsx
@@ -0,0 +1,43 @@
+import { MovieService } from '@/services/movie/movie.service'
+import { errorCatch } from 'api/api.helpers'
+import { Metadata } from 'next'
+import dynamic from 'next/dynamic'
+import Error404 from '../not-found'
+const DynamicChildrenMoviesPage = dynamic(
+	() => import('@/screens/children/ChildrenMoviesPage'),
+	{ ssr: false }
+)
+
+export const metadata: Metadata = {
+	title: 'For children | Online-Cinema'
+}
+
+export const revalidate = 60
+
+const ForChildrenPage = async () => {
+	const data = await staticContent()
+
+	const childrenMovies = data?.childrenMovies
+
+	return childrenMovies ? (
+		<DynamicChildrenMoviesPage childrenMovies={childrenMovies || []} />
+	) : (
+		<Error404 />
+	)
+}
+
+const staticContent = async () => {
+	try {
+		const { data: childrenMovies } = await MovieService.getByGenres([
+			'662170715b5c6107dbf33895'
+		])
+
+		return {
+			childrenMovies
+		}
+	} catch (error) {
+		console.log(errorCatch(error))
+	}
+}
+
+export default ForChildrenPage
