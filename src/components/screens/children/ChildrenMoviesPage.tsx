79e5045be1f6e62c06924436ccd71e693feda88b
nfdmitry <khv1702@gmail.com>
2024-06-04 22:50:37 +0300

    A section "For children" has been implemented

---
+++ src/components/screens/children/ChildrenMoviesPage.tsx
@@ -0,0 +1,25 @@
+import styles from '@/components/shared/contentWrapper.module.scss'
+import { FC } from 'react'
+import CatalogMovies from '../templates/catalog-movies/CatalogMovies'
+import { IChildrenMoviesPage } from './children-movies.interface'
+
+const ChildrenMoviesPage: FC<IChildrenMoviesPage> = ({
+	childrenMovies
+}) => {
+	const title = 'For children'
+	const description =
+		'A selection of the best films and cartoons for children'
+
+	return (
+		<div className={styles.contentWrapper}>
+			<CatalogMovies
+				movies={childrenMovies || []}
+				title={title}
+				description={description}
+				favorite={true}
+			/>
+		</div>
+	)
+}
+
+export default ChildrenMoviesPage
