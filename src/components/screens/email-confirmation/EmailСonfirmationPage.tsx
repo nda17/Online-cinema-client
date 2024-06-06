32273962578032c75062d2206bf6b128277b00b5
nfdmitry <khv1702@gmail.com>
2024-06-07 01:13:42 +0300

    Added redirect to home page after 5 seconds

--- src/components/screens/email-confirmation/EmailСonfirmationPage.tsx
+++ src/components/screens/email-confirmation/EmailСonfirmationPage.tsx
@@ -1,17 +1,27 @@
+'use client'
 import styles from '@/components/shared/contentWrapper.module.scss'
 import Description from '@/ui/description/Description'
 import Heading from '@/ui/heading/Heading'
+import { useRouter } from 'next/navigation'
 import { FC } from 'react'
 
 const EmailСonfirmationPage: FC<{
 	status: string
 }> = ({ status }) => {
 	const title = 'Email confirmation'
+	const { replace } = useRouter()
+
+	setTimeout(() => {
+		replace('/')
+	}, 5000)
 
 	return (
 		<div className={styles.contentWrapper}>
 			<Heading title={title} />
 			<Description text={status} />
+			<p className="text-[#28b54d] italic">
+				Redirect to home page after 5 seconds...
+			</p>
 		</div>
 	)
 }
