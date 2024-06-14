3742caef3862f09392f5ec25452bd043f1351c43
nfdmitry <khv1702@gmail.com>
2024-06-14 18:02:57 +0300

    Added display of an information window in the absence of authorization and email confirmation

--- src/components/screens/single-movie/StarRating/StarRating.tsx
+++ src/components/screens/single-movie/StarRating/StarRating.tsx
@@ -1,8 +1,9 @@
 'use client'
 import { useAuth } from '@/hooks/useAuth'
+import { UserService } from '@/services/user/user.service'
 import MaterialIcon from '@/ui/icons/MaterialIcon'
-import AuthButton from '@/ui/video-player/placeholder-screens/AuthPlaceholder/AuthButton'
 import { FC, useEffect, useState } from 'react'
+import { useQuery } from 'react-query'
 import styles from './StarRating.module.scss'
 import { IStarRating } from './star-rating.interface'
 import { useStarRating } from './useStarRating'
@@ -12,10 +13,18 @@ const StarRating: FC<IStarRating> = ({
 	icon = 'MdStar',
 	defaultStarColor = { color: '#4f4f4f' },
 	activeStarColor = { color: '#ffd700' },
-	slug,
 	_id
 }) => {
 	const { user } = useAuth()
+	const { data: statusConfirmationEmail } = useQuery(
+		'get confirmation email status',
+		() => UserService.getProfile(),
+		{
+			select: (data) => data.data.isActivated,
+			enabled: !!user
+		}
+	)
+
 	const { handleClick, isSended, rating } = useStarRating(_id)
 
 	const stars = Array(count).fill(0)
@@ -25,11 +34,11 @@ const StarRating: FC<IStarRating> = ({
 
 	useEffect(() => {
 		setHoverItem(rating - 1)
-	}, [rating])
+	}, [rating, user, statusConfirmationEmail])
 
 	return (
 		<div className={styles.rating}>
-			{user ? (
+			{user && statusConfirmationEmail ? (
 				<>
 					{isSended ? (
 						<div className={styles.thanks}>Thanks for rating!</div>
@@ -64,7 +73,16 @@ const StarRating: FC<IStarRating> = ({
 					)}
 				</>
 			) : (
-				<AuthButton slug={slug} />
+				<div className="flex justify-center items-center">
+					<p className="text-[1.125rem] leading-normal text-primary">
+						{!user && !statusConfirmationEmail && 'Login to rate the film'}
+					</p>
+					<p className="text-[1.125rem] leading-normal text-primary">
+						{user &&
+							!statusConfirmationEmail &&
+							'Confirm your email to rate the film'}
+					</p>
+				</div>
 			)}
 		</div>
 	)
