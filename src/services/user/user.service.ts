f53f0a2824c0dd36320678796d0b3db4c8052f0f
nfdmitry <khv1702@gmail.com>
2024-06-14 04:22:28 +0300

    Email confirmation service removed

--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -1,13 +1,10 @@
-import { IEmailСonfirmation } from '@/components/screens/email-confirmation/email-confirmation.interface'
 import { PUBLIC_PATH } from '@/configs/api.config'
 import { USER_URL } from '@/configs/url.config'
 import { IUserEditInput } from '@/screens/admin/user/user-edit.interface'
 import { IProfileInput } from '@/screens/profile/profile.interface'
 import { IMovie } from '@/shared/types/movie.types'
 import { IUser } from '@/shared/types/user.types'
-import axiosInterceptorsRequest, {
-	axiosClassicRequest
-} from 'api/interceptors'
+import axiosInterceptorsRequest from 'api/interceptors'
 
 export const UserService = {
 	async getProfile() {
@@ -39,18 +36,6 @@ export const UserService = {
 	async getUserById(_id: string) {
 		return axiosInterceptorsRequest.get<IUserEditInput>(
 			PUBLIC_PATH.usersUrl(`/${_id}`)
-		)
-	},
-
-	async getStatusConfirmationEmail(_id: string) {
-		return axiosClassicRequest.get<IEmailСonfirmation>(
-			PUBLIC_PATH.usersUrl(`/confirmation-email/${_id}`)
-		)
-	},
-
-	async updateStatusConfirmationEmail(_id: string) {
-		return axiosClassicRequest.patch<IEmailСonfirmation>(
-			PUBLIC_PATH.usersUrl(`/confirmation-email/${_id}`)
 		)
 	},
 
