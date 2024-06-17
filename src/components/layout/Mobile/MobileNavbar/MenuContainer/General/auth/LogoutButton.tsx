9b18e7936636e5d9d158fa3ade2bd27d7131f2a0
nfdmitry <khv1702@gmail.com>
2024-06-17 17:45:49 +0300

    Added closing the mobile menu when logging out

--- src/components/layout/Mobile/MobileNavbar/MenuContainer/General/auth/LogoutButton.tsx
+++ src/components/layout/Mobile/MobileNavbar/MenuContainer/General/auth/LogoutButton.tsx
@@ -1,16 +1,25 @@
 import { useActions } from '@/hooks/useActions'
+import { setVisibleHamburger } from '@/store/hamburger/hamburger.slice'
 import MaterialIcon from '@/ui/icons/MaterialIcon'
 import { useRouter } from 'next/navigation'
 import { FC, MouseEvent } from 'react'
+import { useDispatch } from 'react-redux'
 import styles from '../../Menu.module.scss'
 
 const LogoutButton: FC = () => {
 	const { logout } = useActions()
+
+	const dispatch = useDispatch()
+	const closeMenu = () => {
+		dispatch(setVisibleHamburger(false))
+	}
+
 	const { replace } = useRouter()
 
 	const logoutHandler = (e: MouseEvent<HTMLAnchorElement>) => {
 		e.preventDefault()
 		logout()
+		closeMenu()
 		replace('/auth')
 	}
 
