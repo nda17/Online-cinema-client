b8333006a525ce7e18954fb046f4b3c90f8052ef
nfdmitry <khv1702@gmail.com>
2024-06-18 01:46:33 +0300

    Implemented a darkened background component when the mobile menu or menu with selected movies is active

---
+++ src/components/ui/veil-background/VeilBackground.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react'
+import styles from './VeilBackground.module.scss'
+
+const VeilBackground: FC = () => {
+	return <div className={styles.background}></div>
+}
+
+export default VeilBackground
