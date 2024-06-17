0e985c2d66900fdb1e14a98cda2176ebce97f843
nfdmitry <khv1702@gmail.com>
2024-06-17 17:46:43 +0300

    Added closing of the mobile menu and the menu with selected movies when clicking on the logo

--- src/components/layout/Mobile/MobileNavbar/Navigation/Logo/Logo.tsx
+++ src/components/layout/Mobile/MobileNavbar/Navigation/Logo/Logo.tsx
@@ -1,12 +1,22 @@
 import logoImage from '@/assets/images/logo.svg'
+import { setVisibleFavorites } from '@/store/favorites-menu/favorites-menu.slice'
+import { setVisibleHamburger } from '@/store/hamburger/hamburger.slice'
 import Image from 'next/image'
 import Link from 'next/link'
 import { FC } from 'react'
+import { useDispatch } from 'react-redux'
 import styles from './Logo.module.scss'
 
 const Logo: FC = () => {
+	const dispatch = useDispatch()
+
+	const closeMenu = () => {
+		dispatch(setVisibleHamburger(false))
+		dispatch(setVisibleFavorites(false))
+	}
+
 	return (
-		<Link href="/" className={styles.logo}>
+		<Link href="/" className={styles.logo} onClick={closeMenu}>
 			<Image
 				src={logoImage}
 				alt="Online cinema app"
