8b7db0164026643e2d4c62bbf4d5a692bfbed321
nfdmitry <khv1702@gmail.com>
2024-06-22 15:47:39 +0300

    Added additional wrapper

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -11,36 +11,38 @@ const Footer: FC = () => {
 
 	return (
 		<div className={styles.footer}>
-			<div className={styles.social}>
-				<Link href="#0" className={styles.socialIcon}>
-					<FontAwesomeIcon name="FaTelegram" />
-				</Link>
-				<Link href="#0" className={styles.socialIcon}>
-					<FontAwesomeIcon name="FaTwitter" />
-				</Link>
-				<Link href="#0" className={styles.socialIcon}>
-					<FontAwesomeIcon name="FaYoutube" />
-				</Link>
-				<Link href="#0" className={styles.socialIcon}>
-					<MaterialIcon name="MdOutlineMailOutline" />
-				</Link>
-			</div>
-			<div className={styles.info}>
-				<p className={styles.date}>© 2024 - {currentYear}</p>
-				<p className={styles.allRightsReserved}>All rights reserved</p>
-			</div>
-			<div className={styles.content}>
-				<Link href="/" className={styles.logo}>
-					<Image
-						src={logoImage}
-						alt="Online cinema app"
-						draggable={false}
-						priority={true}
-					/>
-				</Link>
-				<div className={styles.linkBlock}>
-					<Link href="/privacy-policy">Privacy policy</Link>
-					<Link href="/cookie-notice">Cookie notice</Link>
+			<div className={styles.wrapper}>
+				<div className={styles.social}>
+					<Link href="#0" className={styles.socialIcon}>
+						<FontAwesomeIcon name="FaTelegram" />
+					</Link>
+					<Link href="#0" className={styles.socialIcon}>
+						<FontAwesomeIcon name="FaTwitter" />
+					</Link>
+					<Link href="#0" className={styles.socialIcon}>
+						<FontAwesomeIcon name="FaYoutube" />
+					</Link>
+					<Link href="#0" className={styles.socialIcon}>
+						<MaterialIcon name="MdOutlineMailOutline" />
+					</Link>
+				</div>
+				<div className={styles.info}>
+					<p className={styles.date}>© 2024 - {currentYear}</p>
+					<p className={styles.allRightsReserved}>All rights reserved</p>
+				</div>
+				<div className={styles.content}>
+					<Link href="/" className={styles.logo}>
+						<Image
+							src={logoImage}
+							alt="Online cinema app"
+							draggable={false}
+							priority={true}
+						/>
+					</Link>
+					<div className={styles.linkBlock}>
+						<Link href="/privacy-policy">Privacy policy</Link>
+						<Link href="/cookie-notice">Cookie notice</Link>
+					</div>
 				</div>
 			</div>
 		</div>
