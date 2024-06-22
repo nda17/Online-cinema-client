b935da8395608f402bcddf8f2bd149e7dc666ec8
nfdmitry <khv1702@gmail.com>
2024-06-22 19:14:07 +0300

    Added display of the proloader when loading dynamic components

--- src/components/layout/Sidebars/RightSidebar/MoviesContainer/MoviesContainer.tsx
+++ src/components/layout/Sidebars/RightSidebar/MoviesContainer/MoviesContainer.tsx
@@ -1,16 +1,28 @@
+import SkeletonLoader from '@/components/ui/skeleton-loader/SkeletonLoader'
 import dynamic from 'next/dynamic'
 import { FC } from 'react'
 import PopularMovies from './PopularMovies/PopularMovies'
 
 const DynamicFavoriteMovies = dynamic(
 	() => import('./FavoriteMovies/FavoriteMovies'),
-	{ ssr: false }
+	{
+		loading: () => (
+			<SkeletonLoader
+				count={3}
+				className="h-[10.83rem] lg:h-[5.98rem] mb-4"
+			/>
+		),
+		ssr: false
+	}
 )
 
 const MoviesContainer: FC = () => {
 	return (
 		<>
 			<PopularMovies />
+			<h1 className="text-gray-500 uppercase text-sm font-semibold mb-8">
+				Favorites
+			</h1>
 			<DynamicFavoriteMovies />
 		</>
 	)
