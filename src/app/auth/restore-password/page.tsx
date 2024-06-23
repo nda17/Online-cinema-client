a341c7e64ef007fee1701d6a0e3ce73b130139f1
nfdmitry <khv1702@gmail.com>
2024-06-23 05:20:31 +0300

    Added dynamic import of the "RestorePassword.tsx" component

--- src/app/auth/restore-password/page.tsx
+++ src/app/auth/restore-password/page.tsx
@@ -1,12 +1,20 @@
-import RestorePassword from '@/screens/auth/restore-password/RestorePassword'
+import SpinnerLoader from '@/ui/spinner-loader/SpinnerLoader'
 import { Metadata } from 'next'
+import dynamic from 'next/dynamic'
+const DynamicRestorePassword = dynamic(
+	() => import('@/screens/auth/restore-password/RestorePassword'),
+	{
+		loading: () => <SpinnerLoader />,
+		ssr: false
+	}
+)
 
 export const metadata: Metadata = {
 	title: 'Restore password | Online-Cinema'
 }
 
 const RestorePasswordPage = () => {
-	return <RestorePassword />
+	return <DynamicRestorePassword />
 }
 
 export default RestorePasswordPage
