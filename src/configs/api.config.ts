57e6e4b3d36e5f2bcd659cec941e09f873fe890c
nfdmitry <khv1702@gmail.com>
2024-07-05 21:48:19 +0300

    Public paths removed

--- src/configs/api.config.ts
+++ src/configs/api.config.ts
@@ -1,11 +1,1 @@
 export const API_URL = `${process.env.APP_URL}/api`
-
-export const PUBLIC_PATH = {
-	usersUrl: (string: string) => `/users${string}`,
-	genresUrl: (string: string) => `/genres${string}`,
-	moviesUrl: (string: string) => `/movies${string}`,
-	actorsUrl: (string: string) => `/actors${string}`,
-	ratingsUrl: (string: string) => `/ratings${string}`
-}
-
-export const getAuthUrl = (string: string) => `/auth${string}`
