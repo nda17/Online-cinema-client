a711aa3381b1ee66e5083ef9c6a1186137ae2ae8
nfdmitry <khv1702@gmail.com>
2024-07-20 15:23:06 +0300

    Edited URL, removed accepted parameter

--- src/components/ui/video-player/placeholder-screens/AuthPlaceholder/AuthButton.tsx
+++ src/components/ui/video-player/placeholder-screens/AuthPlaceholder/AuthButton.tsx
@@ -1,14 +1,11 @@
-import { PUBLIC_PAGES } from '@/configs/pages/public.config'
+import { getAdminHomeUrl } from '@/configs/url.config'
 import Link from 'next/link'
 import { FC } from 'react'
 import styles from './AuthPlaceholder.module.scss'
 
-const AuthButton: FC<{ slug: string }> = ({ slug }) => {
+const AuthButton: FC = () => {
 	return (
-		<Link
-			href={`/auth?redirect=/${PUBLIC_PAGES.MOVIES}/${slug}}`}
-			className={styles.button}
-		>
+		<Link href={getAdminHomeUrl()} className={styles.button}>
 			Sign in
 		</Link>
 	)
