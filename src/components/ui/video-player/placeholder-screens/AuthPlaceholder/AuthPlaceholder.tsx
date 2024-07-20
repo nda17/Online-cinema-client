aa247e56a035d48c55cd325711e79ae37a631101
nfdmitry <khv1702@gmail.com>
2024-07-20 15:23:58 +0300

    Removed slug passed property

--- src/components/ui/video-player/placeholder-screens/AuthPlaceholder/AuthPlaceholder.tsx
+++ src/components/ui/video-player/placeholder-screens/AuthPlaceholder/AuthPlaceholder.tsx
@@ -2,12 +2,12 @@ import { FC } from 'react'
 import AuthButton from './AuthButton'
 import styles from './AuthPlaceholder.module.scss'
 
-const AuthPlaceholder: FC<{ slug: string }> = ({ slug }) => {
+const AuthPlaceholder: FC = () => {
 	return (
 		<div className={styles.placeholder}>
 			<div>
 				<div>You must be logged in to start watching</div>
-				<AuthButton slug={slug} />
+				<AuthButton />
 			</div>
 		</div>
 	)
