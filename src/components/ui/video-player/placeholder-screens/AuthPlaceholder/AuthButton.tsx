ef105c55517a0966809f97f3de82bb7e0182e16b
nfdmitry <khv1702@gmail.com>
2024-07-21 01:35:34 +0300

    Fixing a bug with a request

--- src/components/ui/video-player/placeholder-screens/AuthPlaceholder/AuthButton.tsx
+++ src/components/ui/video-player/placeholder-screens/AuthPlaceholder/AuthButton.tsx
@@ -1,11 +1,11 @@
-import { getAdminHomeUrl } from '@/configs/url.config'
+import { getAuthUrl } from '@/configs/api.config'
 import Link from 'next/link'
 import { FC } from 'react'
 import styles from './AuthPlaceholder.module.scss'
 
 const AuthButton: FC = () => {
 	return (
-		<Link href={getAdminHomeUrl()} className={styles.button}>
+		<Link href={getAuthUrl(``)} className={styles.button}>
 			Sign in
 		</Link>
 	)
